1dc524faec38434d333fef79a65ab5813bcd1184
jstnknr <143372221+jstnknr@users.noreply.github.com>
2024-02-08 10:33:07 -0500

    Questions appear and responses recorded.

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
-// Your code here
-
-
-function runQuiz() {
+function runQuiz () {
     var questionsArr = [
-            {
-                question: 'The color of the sky is blue.',
-                answer: true
-            }
-            {
-                question: 'Wednesday is the day after Monday',
-                answer: false
-            }
-            {
-                question: 'Yellow is the complimentary color of Indigo.',
-                answer:true
-            }
-            {
-                question: 'The first president of the United States was John Adams.',
-                answer: false
-            }
-            {
-                question: 'There are 13 inches in a foot.',
-                answer: false
-            }       
-        ]
-        for (var i = 0; i < questionsArr.length; i++) {
-            var questionsArr = questions[i]
-            console.log(questionsArr)
-            var answer = confirm(questionsArr)
-        }
+        "The sky is blue.",
+        "Wednesday is the day after Monday.",
+        "Yellow is the complimentary color of Indigo.",
+        "The first president of the United States was John Adams.",
+        "There are 13 inches in a foot."
+    ]
+    for (var i = 0; i < questionsArr.length; i++) {
+        var question = questionsArr[i]
+        var answer = confirm(question)
+        console.log(answer)
+    }
 }
+
+//{
+//    question: 'The color of the sky is blue.',
+//    answer: true
+//   }
+//   {
+//    question: 'Wednesday is the day after Monday',
+//    answer: false
+ //  }
+ //  {
+ //   question: 'Yellow is the complimentary color of Indigo.',
+ //   answer: true
+ //  }
+ //  {
+  //  question: 'The first president of the United States was John Adams.',
+//    answer: false
+//   }
+//   {
+//    question: 'There are 13 inches in a foot.',
+//    answer: false
+//   }
